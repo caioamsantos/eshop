c95cf97a303b884cfc3878ded5f75c7601df4598
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-05-16 13:16:36 -0300

    criação de models

---
+++ src/app/models/action.model.ts
@@ -0,0 +1,6 @@
+﻿import { Action } from '@ngrx/store';
+
+export class ActionModel implements Action {
+    type: string;
+    payload: any;
+}
---
+++ src/app/models/cart.model.ts
@@ -0,0 +1,6 @@
+﻿import { ProductModel } from './product.model';
+
+export class CartModel {
+    public products: ProductModel[] = [];
+    public total = 0;
+}
