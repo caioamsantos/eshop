311ced5b928f268c8003731b28ad9a75bb276441
caioamsantos <54689983+caioamsantos@users.noreply.github.com>
2020-05-17 17:00:36 -0300

    criação do componrnte product list

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { CartModel } from 'src/app/models/cart.model';
+import { ToastController } from '@ionic/angular';
+import { DataService } from 'src/app/data.service';
+import { Store } from '@ngrx/store';
+import { Add } from 'src/app/actions/cart.action';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent implements OnInit {
+  public products: any[] = null;
+
+  constructor(
+    private store: Store<CartModel>,
+    private toastCtrl: ToastController,
+    private service: DataService
+  ) { }
+
+  ngOnInit() {
+    this.service
+      .getProducts()
+      .subscribe((data) => {
+        this.products = data;
+      });
+   }
+
+  async add(product){
+    this.store.dispatch(Add(product));
+
+    const toast = await this.toastCtrl.create({
+      message: `${product.title} adicionado ao carrinho`,
+      duration: 2000,
+      buttons: [
+        {
+          text: 'OK',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
+}
